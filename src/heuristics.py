40440ddb47d77917cf9f62c8e698c7eda11b4053
radsfer <rafael.ferreira11.98@gmail.com>
2025-10-18 16:11:25 -0300

    estrutura

---
+++ src/heuristics.py
@@ -0,0 +1,14 @@
+# src/heuristics.py
+from typing import Tuple
+
+Pos = Tuple[int, int]
+
+def h_manhattan(a: Pos, b: Pos) -> float:
+    (x1, y1) = a
+    (x2, y2) = b
+    return abs(x1 - x2) + abs(y1 - y2)
+
+def h_euclidiana(a: Pos, b: Pos) -> float:
+    (x1, y1) = a
+    (x2, y2) = b
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5

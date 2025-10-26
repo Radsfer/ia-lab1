646d90443ca6634375fd3afc40bd164dfd0fd4d8
radsfer <rafael.ferreira11.98@gmail.com>
2025-10-26 14:36:10 -0300

    formatando estrutura do segundo trabalho

--- trabalho1/src/heuristics.py
+++ trabalho1/src/heuristics.py
@@ -12,9 +12,7 @@ def h_euclidiana(a: Pos, b: Pos) -> float:
     (x2, y2) = b
     return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
-# --- NOVA HEURÍSTICA ---
 def h_diagonal(a: Pos, b: Pos) -> float:
-    """Calcula a Distância Diagonal (Chebyshev)."""
     (x1, y1) = a
     (x2, y2) = b
     return max(abs(x1 - x2), abs(y1 - y2))
